57ef7860b620d887154bf32d3550a842144e0770
Flávio <flavio.barbosa@sinch.com>
2022-09-03 18:05:13 -0300

    wip

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,14 +1,16 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class IndexController {
 
 	@GetMapping("/cliente")
-	public void getCliente() {
-		ModelAndView mv = new ModelAndView(viewName:"index");
-		
+	public ModelAndView getCliente() {
+		ModelAndView mv = new ModelAndView("index");
 		return mv;
 	}
 	
