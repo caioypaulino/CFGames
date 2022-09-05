ec7ef010519e1d2fac3dd996c4dddcef5c60c890
Caio Paulino <caiohenriquepaulino@gmail.com>
2022-09-05 00:23:19 -0300

    Crud JPA (Corrigido warning)

--- src/test/java/com/project/cfgames/CfGamesApplicationTests.java
+++ src/test/java/com/project/cfgames/CfGamesApplicationTests.java
@@ -1,11 +1,7 @@
 package com.project.cfgames;
 
-import java.util.Date;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.project.cfgames.entities.Cliente;
 
 @SpringBootTest
 class CfGamesApplicationTests {
