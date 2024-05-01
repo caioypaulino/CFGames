60d4039e96d8b21660edee0b37e5a957d2d147a0
Caio Paulino <caiohenriquepaulino@gmail.com>
2024-05-01 18:28:51 -0300

    genero response

--- src/main/java/com/project/cfgames/dtos/responses/ClienteResponse.java
+++ src/main/java/com/project/cfgames/dtos/responses/ClienteResponse.java
@@ -1,6 +1,7 @@
 package com.project.cfgames.dtos.responses;
 
 import com.project.cfgames.entities.Perfil;
+import com.project.cfgames.entities.enums.Genero;
 import com.project.cfgames.entities.relations.EnderecoCliente;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +18,7 @@ public class ClienteResponse {
     private String nome;
     private String cpf;
     private LocalDate dataNascimento;
+    private Genero genero;
     private String telefone;
     private String email;
     private String senha;
